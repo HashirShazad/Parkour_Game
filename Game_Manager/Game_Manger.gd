53d5d96f7315247bdb31abf20eae1485a39e244f
Hashir Shazad <hashir447079@gmail.com>
2024-03-23 14:28:24 +0500

    `added gui

--- Game_Manager/Game_Manger.gd
+++ Game_Manager/Game_Manger.gd
@@ -1,10 +1,18 @@
 extends Node
 
-@onready var points_label = $"../UI/Panel/PointsLabel"
+
+@onready var points_label = $"../UI/Node/Coin_Info_Box/PointsLabel"
 
 var points = 0
 
 func add_points(collected_points:int) -> void:
 	points = points + collected_points
 	print(points)
-	points_label.text = "Points:" + str(points)
+	points_label.text = "Points: " + str(points)
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Levels/Level_1.tscn")
+
+
+func _on_back_button_pressed():
+	get_tree().quit()
