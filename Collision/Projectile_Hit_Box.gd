1ba7698df269a3424ee879bf4d44b1da059b5d37
Hashir Shazad <hashir447079@gmail.com>
2024-04-23 13:27:53 +0500

    adads
    dasd

--- Collision/Projectile_Hit_Box.gd
+++ Collision/Projectile_Hit_Box.gd
@@ -9,13 +9,9 @@ class_name Projectile_Hit_Box
 func _init() -> void:
 	collision_layer = 2
 	collision_mask = 2
-	#sprite_2d.texture = texture
 	
-func _ready():
-	if sprite_2d.flip_h == true:
-		direction = 1
-	elif sprite_2d.flip_h == false:
-		direction = -1
+
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position.x += speed * direction * delta
